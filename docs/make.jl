844e7a51194b4cfc4a9a99705a563290cdd94943
martinkosch <martin.kosch@gmail.com>
2021-08-28 00:23:46 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ DocMeta.setdocmeta!(open62541, :DocTestSetup, :(using open62541); recursive=true
 
 makedocs(;
     modules=[open62541],
-    authors="Martin Kosch <martin.kosch@rwth-aachen.de> and contributors",
-    repo="https://github.com/Martin Kosch/open62541.jl/blob/{commit}{path}#{line}",
+    authors="Martin Kosch <martin.kosch@gmail.com> and contributors",
+    repo="https://github.com/martinkosch/open62541.jl/blob/{commit}{path}#{line}",
     sitename="open62541.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Martin Kosch.github.io/open62541.jl",
+        canonical="https://martinkosch.github.io/open62541.jl",
         assets=String[],
     ),
     pages=[
@@ -19,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/Martin Kosch/open62541.jl",
+    repo="github.com/martinkosch/open62541.jl",
 )
