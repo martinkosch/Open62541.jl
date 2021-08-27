4d3c67a38c7a200498f4171691f8b8bd1c51e9ce
Documenter.jl <documenter@juliadocs.github.io>
2021-08-27 22:48:46 +0000

    build based on 55d35b0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = open62541","category":"page"},{"location":"#open62541","page":"Home","title":"open62541","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for open62541.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [open62541]","category":"page"}]
+}
