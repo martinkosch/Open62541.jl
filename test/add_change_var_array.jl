c15af8f9feb1d5eff7de7fd722731352bb60c917
Thomas Vetter <80452158+thomvet@users.noreply.github.com>
2023-11-30 16:29:13 +0100

    ...

--- test/add_change_var_array.jl
+++ test/add_change_var_array.jl
@@ -9,9 +9,9 @@ using Base.Threads
 
 # What types we are testing for: 
 #types = [Int16, Int32, Int64, Float32, Float64, Bool]
-types = [Bool, Float64, Int64, Int32]
+types = [Float64, Int64]
 #array_sizes = (11, (2, 5), (3, 4, 5), (3, 4, 5, 6))
-array_sizes = reverse((11, (2, 3), (2, 3, 4), (2, 3, 4, 5)))
+array_sizes = (11, (2, 3), (2, 3, 4), (2, 3, 4, 5))
 
 for type in types
     for array_size in array_sizes
