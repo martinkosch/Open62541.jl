ac93d7e2c189a97488d9cefbed9535e3bc6b456e
Thomas Vetter <80452158+thomvet@users.noreply.github.com>
2023-11-30 15:09:37 +0100

    4d array

--- test/add_change_var_array.jl
+++ test/add_change_var_array.jl
@@ -9,7 +9,7 @@ using Base.Threads
 
 # What types we are testing for: 
 #types = [Int16, Int32, Int64, Float32, Float64, Bool]
-types = [Float32, Float64, Bool]
+types = [Float64, Bool]
 #array_sizes = (11, (2, 5), (3, 4, 5), (3, 4, 5, 6))
 array_sizes = (11, (2, 5), (3, 4, 5), (3, 4, 5, 6))
 
