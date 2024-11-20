a9ec4fdf6cd0ef336a829f6463dc37a01909ebff
thomvet <thvt@lundbeck.com>
2024-11-20 17:43:32 +0100

    bugfixes and simplifications; tests passing

--- test/client_subscriptions.jl
+++ test/client_subscriptions.jl
@@ -1,7 +1,10 @@
 #adapted from: https://github.com/open62541/open62541/blob/master/examples/client_subscription_loop.c
+#the example (as of 2024-11-20) wants to create a subscription from within the 
+#callback, which is not allowed, see here:
+#https://github.com/open62541/open62541/issues/5816
+#Unmerged PR addressing this: https://github.com/open62541/open62541/pull/5905
 
 using Distributed
-using Printf
 Distributed.addprocs(1) # Add a single worker process to run the server
 
 Distributed.@everywhere begin
@@ -13,7 +16,7 @@ Distributed.@spawnat Distributed.workers()[end] begin
     #configure the server
     server = UA_Server_new()
     retval = UA_ServerConfig_setMinimalCustomBuffer(UA_Server_getConfig(server),
-        4842, C_NULL,  0, 0)
+        4843, C_NULL,  0, 0)
 
     # Start up the server
     Distributed.@spawnat Distributed.workers()[end] redirect_stderr() # Turn off all error messages
@@ -27,17 +30,12 @@ config = UA_Client_getConfig(client)
 UA_ClientConfig_setDefault(config)
 
 #define callbacks
-function handler_currentTimeChanged(client, subId, subContext, monId, monContext, 
+function handler_simple(client, subId, subContext, monId, monContext, 
         value)
-    if UA_Variant_hasScalarType(value.value, UA_TYPES_PTRS[UA_TYPES_DATETIME])
-        raw_date = unsafe_wrap(value.value)
-        dts = UA_DateTime_toStruct(raw_date)
-        push!(container, Printf.@sprintf("current date and time (UTC) is: %u-%u-%u %u:%u:%u.%03u\n",
-        dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec))
-    end
+    push!(container, rand()) #we just collect a bunch of random numbers here.
     return nothing
 end
-handlercb = @cfunction(handler_currentTimeChanged, Cvoid, (Ptr{UA_Client}, UInt32, Ptr{Cvoid}, UInt32, Ptr{Cvoid}, UA_DataValue))
+handlercb = @cfunction(handler_simple, Cvoid, (Ptr{UA_Client}, UInt32, Ptr{Cvoid}, UInt32, Ptr{Cvoid}, UA_DataValue))
 
 #connect the client
 max_duration = 90.0 # Maximum waiting time for server startup 
@@ -45,7 +43,7 @@ sleep_time = 3.0 # Sleep time in seconds between each connection trial
 let trial
     trial = 0
     while trial < max_duration / sleep_time
-        retval = UA_Client_connect(client, "opc.tcp://localhost:4842")
+        retval = UA_Client_connect(client, "opc.tcp://localhost:4843")
         if retval == UA_STATUSCODE_GOOD
             println("Connection established.")
             break
@@ -68,8 +66,9 @@ monResponse = UA_Client_MonitoredItems_createDataChange(client, response.subscri
     C_NULL, handlercb, C_NULL)
             
 #now interrogate the thing
-UA_Client_run_iterate(client, 1000) 
-container = String[]
+container = Float64[] #need to initialize variable here, otherwise error (use in handler_currentTimeChanged(...))
+UA_Client_run_iterate(client, 1000)
+container = Float64[] 
 sleep(7) 
 UA_Client_run_iterate(client, 1000)
 #see UA_CreateSubscriptionRequest_default(); 
@@ -80,3 +79,7 @@ UA_Client_run_iterate(client, 1000)
 
 UA_Client_disconnect(client)
 UA_Client_delete(client) 
+
+println("Ungracefully kill server process...")
+Distributed.interrupt(Distributed.workers()[end])
+Distributed.rmprocs(Distributed.workers()[end]; waitfor = 0)
